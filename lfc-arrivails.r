004d10da14fc54f114b9c912b8165cf5dd8fa2dc
mooromets <sergey.sambor@gmail.com>
2017-08-08 11:06:31 +0200

    top-3 transfers added

--- lfc-arrivails.r
+++ lfc-arrivails.r
@@ -8,14 +8,32 @@ data <- data %>%
   mutate(Date = dmy(Date))
 
 dates <- data %>%
-  select(Date) %>%
+  select(Date, Player, Fee) %>%
   filter(between(month(Date), 5, 10 )) %>% #summer transfer window
-  mutate (Date = as.Date(format(Date, "2017/%m/%d"), "%Y/%m/%d")) #make all month-day pairs comparable
+  mutate (Fee = ifelse(gsub("[^[:digit:]]", "", Fee) == "", 0, as.integer(gsub("[^[:digit:]]", "", Fee)) / 10^6),
+          Year = year(Date), 
+          Date = as.Date(format(Date, "2017/%m/%d"), "%Y/%m/%d")) #make all month-day pairs comparable 
 
 qplot(dates$Date)
 
-# percent of transfers
+# percent of transfers till the end
 sum(dates$Date > today())/length(dates$Date) 
 
-# number of transfers per season
+# number of transfers per season till the end
 sum(dates$Date > today())/(2017 - 2004) 
+
+# top 3 transfers
+top <- dates %>% group_by(Year) %>% top_n(n=3, wt=Fee)
+# percent of transfers till the end
+sum(top$Date > today())/length(top$Date) 
+
+# number of transfers per season till the end
+sum(top$Date > today())/(2017 - 2004) 
+
+qplot(top$Date)
+
+#number of top transfers per season till the end
+top %>%
+  filter(Date > today()) %>%
+  group_by(Year) %>%
+  summarise(n = n(), sum = sum(Fee) )
